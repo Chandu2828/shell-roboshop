2115a1868cc8d3c183156380e495f22c818a2a18
Chandu2828 <purnachand7569@gmail.com>
2026-05-31 16:43:11 +0530

    Added files

--- 04-rabbitmq.sh
+++ 04-rabbitmq.sh
@@ -39,6 +39,6 @@ VALIDATE $? "Enabling and starting rabbitmq server"
 
 rabbitmqctl add_user roboshop roboshop123 &>> $LOGS_FILE
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>> $LOGS_FILE
-VALIDATE $? "setting up username and passord"
+VALIDATE $? "setting up username and password"
 
 
--- 06-user.sh
+++ 06-user.sh
@@ -15,16 +15,16 @@ N="\e[0m"
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
 if [ $USERID -ne 0 ]; then
-    echo -e "TIMESTAMP[ERROR] $R Please run this script with root access $N" | tee -a $LOGS_FILE
+    echo -e "$TIMESTAMP[ERROR] $R Please run this script with root access $N" | tee -a $LOGS_FILE
     exit 1
 fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo -e "TIMESTAMP [ERROR] $2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        echo -e "$TIMESTAMP [ERROR] $2 ... $R FAILURE $N" | tee -a $LOGS_FILE
         exit 1
     else 
-        echo -e "TIMESTAMP [INFO] $2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+        echo -e "$TIMESTAMP [INFO] $2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
     fi
 }
 
