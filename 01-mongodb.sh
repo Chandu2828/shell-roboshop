810534c0139d923e6899e1e94934561304540e12
Chandu2828 <purnachand7569@gmail.com>
2026-05-31 12:52:20 +0530

    Added files

--- 01-mongodb.sh
+++ 01-mongodb.sh
@@ -36,7 +36,7 @@ VALIDATE $? "Installing MongoDB"
 systemctl enable --now mongod
 VALIDATE $? "Starting and enabling MongoDB"
 
-sed -i 's/127.0.0.0/0.0.0.0/g' /etc/mongod.conf
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "Allowing remote connections to MongoDB"
 
 systemctl restart mongod
